f8fb089d650fc07e08d08caaeb21cc60851bb744
hichemKARFA <39911699+hichemKARFA@users.noreply.github.com>
2021-10-24 22:48:49 +0200

    Add files via upload

---
+++ Roue.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Roue {
+    int team =0;
+
+    ArrayList<Player> resultat = new ArrayList();
+
+    public void playRoue(ArrayList<Player> players){
+        while (!players.isEmpty()){
+            Player winner= Match.playMatch(players);
+            players.remove(winner);
+            team++;
+            winner.setTeam(team%2);
+            winner.setRole(Role.values()[team%(Role.values().length)]);
+            resultat.add(winner);
+        }
+        this.displayWinner(resultat);
+    }
+
+    public void displayWinner(ArrayList<Player> players){
+        int j =1;
+        for (Player p1:players) {
+            System.out.println(p1.getName()+ " -> " + p1.getRole().name()+ " -> Team : "+p1.getTeam());
+            j++;
+        }
+    }
+
+
+}
