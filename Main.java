9c7fa4d3ec2b1062e4ae1f930b15c92f2a90302f
hichemKARFA <39911699+hichemKARFA@users.noreply.github.com>
2021-10-24 22:48:27 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Player> liste = new ArrayList<>();
+        for (int index = 0; index < args.length; index ++) {
+            liste.add(new Player(args[index].toString()));
+        }
+        Roue roue = new Roue();
+        roue.playRoue(liste);
+    }
+}
